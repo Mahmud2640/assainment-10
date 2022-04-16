7a1f0cfb5d2c86be5ffa76341b492d38a19474ac
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-17 03:53:05 +0600

    services

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Services from '../Services/Services';
-
+import React from "react";
+import Services from "../Services/Services";
 
 const Home = () => {
-   return (
-      <div>
-         <Services></Services>
-      </div>
-   );
+  return (
+    <div>
+      <Services></Services>
+    </div>
+  );
 };
 
 export default Home;
